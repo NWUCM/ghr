2cc8895575320ee53db74b061dce07ab8042227e
guohuiran <2106323004@qq.com>
2021-09-24 20:48:21 +0800

    add error

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ h = 0.01
 yn = 1
 tn = 0
 def f(tn, yn):
-    return (4*tn*yn**0.5)
+    return 4*tn*yn**0.5
+def y(tn):
+    return (1 + tn**2)**2   
 for i in range(10):
     yn1 = yn + h * f(tn, yn)
+    exc_y = y(tn)
     tn = tn + h
-    print(yn1)
+    error = yn1 - yn
+    print(error)
+
     
+
 
