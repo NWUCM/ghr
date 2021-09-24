c0749b4b33fbf2e5d9ce21fdc8e1f038a2573ef5
guohuiran <2106323004@qq.com>
2021-09-24 20:15:29 +0800

    add python

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello, word!")
+h = 0.01
+yn = 1
+tn = 0
+def f(tn, yn):
+    return (4*tn*yn**0.5)
+for i in range(10):
+    yn1 = yn + h * f(tn, yn)
+    tn = tn + h
+    print(yn1)
+    
+
